d988ca22e999a5d2172331ebffe21b3c8fc227e1
Core <aukwat@gmail.com>
2021-09-22 17:46:14 +0700

    Update types.dart

--- lib/types/types.dart
+++ lib/types/types.dart
@@ -56,11 +56,7 @@ class TextNode extends Node<String> {
   TextNode(dynamic node) {
     value = node['value'] ?? '';
     _nodeType = node['nodeType'] ?? '';
-    if (node['marks'] != null) {
-      node['marks'].forEach((mark) {
-        marks.add(Mark(mark['type']));
-      });
-    }
+    marks = node['marks']?.map<Mark>((mark) => Mark(mark['type'])).toList() ?? [];
   }
 }
 
